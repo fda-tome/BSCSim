6e252cc8b8017d4ec158c37eaac93af6099135a7
Felipe de Alcântara Tomé <tomefelipe0@usp.br>
2022-12-20 22:02:36 -0300

    full test with expanded precision

--- src/func_test/compare_vl.jl
+++ src/func_test/compare_vl.jl
@@ -71,16 +71,16 @@ function compare!(func::Function,  arrmat::Matrix{BigFloat}, arrjl::Matrix{BigFl
 end
 
 
-setprecision(166)
+setprecision(1661)
 arrmat = Matrix{BigFloat}(undef, 25, 10000)
 arrjl = Matrix{BigFloat}(undef, 25, 10000)
 compare!(shortsphebess!, arrmat, arrjl, 25, 10000, "sphe_bess_curto_mat.csv")
 arrmat = Matrix{BigFloat}(undef, 25, 100000)
 arrjl = Matrix{BigFloat}(undef, 25, 100000)
-compare!(longsphebess!, arrmat, arrjl, 25, 100000, "sphe_bess_longo_mat.csv")
+#=compare!(longsphebess!, arrmat, arrjl, 25, 100000, "sphe_bess_longo_mat.csv")
 arrmat = Matrix{BigFloat}(undef, 9, 10000)
 arrjl = Matrix{BigFloat}(undef, 9, 10000)
-compare!(shortbess!, arrmat, arrjl, 9 , 10000, "bess_curto_mat.csv")
+compare!(shortbess!, arrmat, arrjl, 9 , 10000, "bess_curto_mat.csv")=#
 arrmat = Matrix{BigFloat}(undef, 9, 100000)
 arrjl = Matrix{BigFloat}(undef, 9, 100000)
 compare!(longbess!, arrmat, arrjl, 9, 100000, "bess_longo_mat.csv")
--- src/naive/naive_implem.jl
+++ src/naive/naive_implem.jl
@@ -1,3 +1,10 @@
-function BSC(phi::Function)
-    (factorial(n-m) * (2* n + 1))/(4 * pi * factorial(n + m) )
+using Bessels
+function sphericalBessel()
+    
+end
+
+
+function BSC(phi::Function, k, r)
+    (factorial(n-m) * (2* n + 1))/(4 * pi * factorial(n + m) * besselj(k * r))
+end
 
