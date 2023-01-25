46011a633f19775f370555ab24612f45196a30f0
Felipe de Alcântara Tomé <tomefelipe0@usp.br>
2023-01-24 23:10:02 -0300

    full test with expanded precision

--- src/naive/naive_implem.jl
+++ src/naive/naive_implem.jl
@@ -1,5 +1,16 @@
 using SpecialFunctions
 using AssociatedLegendrePolynomials
+setprecision(167)
 
-function besbeam(psiamp, axiconang, order, z, phi)
+
+function besbeam(psiamp, axiconang, order, rho, phi, z) 
+    k = (2 * pi) / big(1.54/1000) #fixed wavelength 1.54mm
+    kz = k * cosd(axiconang)
+    krho = k * sind(axiconang)
+    return amp * besselj(ord, krho * rho) * exp(ord * im * phi) * exp(kz * im * z)
+end
+
+function makeplot
+ 
+
         
