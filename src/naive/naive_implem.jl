be51d7130598c887fe02d5bf6ceb0462f8310a12
Felipe de Alcântara Tomé <tomefelipe0@usp.br>
2023-01-25 23:06:32 -0300

    working on plotting heatmap

--- src/naive/naive_implem.jl
+++ src/naive/naive_implem.jl
@@ -1,16 +1,27 @@
 using SpecialFunctions
 using AssociatedLegendrePolynomials
+using Plots
 setprecision(167)
 
 
 function besbeam(psiamp, axiconang, order, rho, phi, z) 
-    k = (2 * pi) / big(1.54/1000) #fixed wavelength 1.54mm
-    kz = k * cosd(axiconang)
-    krho = k * sind(axiconang)
-    return amp * besselj(ord, krho * rho) * exp(ord * im * phi) * exp(kz * im * z)
+    k = big(2 * pi) / big(1.54/1000) #fixed wavelength 1.54mm
+    kz = k * big(cosd(axiconang))
+    krho = k * big(sind(axiconang))
+    return big(psiamp * besselj(order, krho * rho) * exp(order * im * phi) * exp(kz * im * z))
 end
 
-function makeplot
- 
+function makeplotbes(nx, ny, ry, rx, psiamp)
+    z = 0
+    x = range(-rx, rx, nx)
+    y = range(-ry, ry, ny)
+    axang = [deg2rad(1), deg2rad(10), deg2rad(40)]
+    ord = [0, 1, 5]
+    htmaps = [(heatmap([(abs(besbeam(psiamp, big(axiconang), order, big(sqrt(i^2 + j^2)), big(acos(i / big(sqrt(i^2 + j^2)))), z))) for i in x, j in y])) for order in ord, axiconang in axang]
+    gr()
+    display(plot(vec(htmaps)..., layout = (3, 3)))
+end
 
-        
+makeplotbes(200, 200, 20, 20, 1)
+
+
