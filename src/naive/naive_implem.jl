1eb0fa5cd90795bee41ab33bfbebaff3ba4b16ac
Felipe de Alcântara Tomé <tomefelipe0@usp.br>
2023-01-26 23:17:14 -0300

    heatmaps plotted for base function

--- src/naive/naive_implem.jl
+++ src/naive/naive_implem.jl
@@ -1,11 +1,9 @@
 using SpecialFunctions
 using AssociatedLegendrePolynomials
 using Plots
-setprecision(167)
-
 
 function besbeam(psiamp, axiconang, order, rho, phi, z) 
-    k = big(2 * pi) / big(1.54/1000) #fixed wavelength 1.54mm
+    k = 1000 * 2 * big(pi) / big(1.54) #fixed wavelength 1.54mm
     kz = k * big(cosd(axiconang))
     krho = k * big(sind(axiconang))
     return big(psiamp * besselj(order, krho * rho) * exp(order * im * phi) * exp(kz * im * z))
@@ -17,9 +15,25 @@ function makeplotbes(nx, ny, ry, rx, psiamp)
     y = range(-ry, ry, ny)
     axang = [deg2rad(1), deg2rad(10), deg2rad(40)]
     ord = [0, 1, 5]
-    htmaps = [(heatmap([(abs(besbeam(psiamp, big(axiconang), order, big(sqrt(i^2 + j^2)), big(acos(i / big(sqrt(i^2 + j^2)))), z))) for i in x, j in y])) for order in ord, axiconang in axang]
-    gr()
-    display(plot(vec(htmaps)..., layout = (3, 3)))
+    htmaps = Vector{Any}()
+    for order in ord, axiconang in axang
+        beamaux = Vector{BigFloat}()
+        for j in y, i in x
+            rho =  sqrt(big(i)^2 + big(j)^2)
+            if j > 0
+                phi = big(acos(big(i) / rho))
+            else
+                if i > 0
+                    phi = 2 * pi - big(acos(big(i) / rho))
+                else
+                    phi = pi +  big(acos(-big(i) / rho))
+                end
+            end
+            push!(beamaux,abs(besbeam(psiamp, big(axiconang), order, rho, phi, z)))
+        end
+        push!(htmaps, heatmap(x, y,  reshape(beamaux, (nx, ny))))
+    end
+    savefig(plot(htmaps..., layout = (3, 3)), "plot.png")
 end
 
 makeplotbes(200, 200, 20, 20, 1)
